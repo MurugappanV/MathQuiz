e3c1e76a38f1eb057ef2ceb0000bb5e3c753301a
Murugappan Viswanathan <murugappanviswanathan@gmail.com>
2020-07-19 14:52:04 +0530

    counter api added

--- flask/quiz.py
+++ flask/quiz.py
@@ -1,13 +1,22 @@
 from flask import Flask
+import import_ipynb
+from src import newfile
+from os import path, walk
+
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/bye')
+@app.route('/new')
 def hey():
     return 'hey, World!'
 
+@app.route('/counter')
+def counter():
+    count = 5
+    return newfile.getCounter()
+
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(extra_files=extra_files)
